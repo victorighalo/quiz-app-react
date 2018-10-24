6f19f099b001e76bc8b2bd7e14d2625551470a8b
IT Executive <victorighalo@live.com>
2018-10-24 18:08:17 +0100

    Updates

--- src/index.js
+++ src/index.js
@@ -3,6 +3,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-
+// import 'bootstrap';
 
 ReactDOM.render(<App />, document.getElementById('root'));
